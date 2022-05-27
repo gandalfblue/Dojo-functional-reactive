5e75d58abfddfba9cb85caef1c78fd14a9f68bd4
dannielf <danielgranados1992@gmail.com>
2022-05-26 19:18:04 -0500

    [Add] exercise 3 derivar done

--- src/main/java/co/com/sk/exercise_3/Exercise.java
+++ src/main/java/co/com/sk/exercise_3/Exercise.java
@@ -1,15 +1,20 @@
 package co.com.sk.exercise_3;
 
 import java.util.Scanner;
+import java.util.function.BiFunction;
 import java.util.logging.Logger;
 
+/**
+ * Exercise 3 class
+ *
+ * @author dannielf - gandalfblue - duvis07 - jeyson-pereira
+ * @version 0.0.1
+ * @since 0.0.1
+ */
 public class Exercise {
 
     private static final Logger log = Logger.getLogger("");
 
-    //    public  static Double derivar(Double x, Double y){
-//
-//    }
     public static void main(String[] args) {
 
         Scanner valueUser = new Scanner(System.in);
@@ -17,5 +22,9 @@ public class Exercise {
         var numberA = valueUser.nextDouble();
         log.info("Ingrese un Double como valor B");
         var numberb = valueUser.nextDouble();
+
+        BiFunction<Double, Double, Double> derivar = Math::pow;
+
+        log.info("Resultado es: " + derivar.apply(numberA, numberb));
     }
 }
