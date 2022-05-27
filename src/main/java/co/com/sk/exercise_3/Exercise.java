ce111ff8bc99da4e790ea0523b9297f2da733d05
dannielf <danielgranados1992@gmail.com>
2022-05-26 19:06:51 -0500

    [Add] exercise 3 in progress

--- src/main/java/co/com/sk/exercise_3/Exercise.java
+++ src/main/java/co/com/sk/exercise_3/Exercise.java
@@ -1,4 +1,21 @@
 package co.com.sk.exercise_3;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Exercise {
+
+    private static final Logger log = Logger.getLogger("");
+
+    //    public  static Double derivar(Double x, Double y){
+//
+//    }
+    public static void main(String[] args) {
+
+        Scanner valueUser = new Scanner(System.in);
+        log.info("Ingrese un Double como valor A");
+        var numberA = valueUser.nextDouble();
+        log.info("Ingrese un Double como valor B");
+        var numberb = valueUser.nextDouble();
+    }
 }
